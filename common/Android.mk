b73aa970591e3f9e5db1dad4182d4d9e16ffa31b
Nolen Johnson <johnsonnolen@gmail.com>
2018-03-17 17:29:17 -0400

    atv: Add SetupWraith & remove non-working GApps
    * As of 8.0, SetupWraith no longer requires Platform signature, so we can move it to GApps
    * AtvRemoteService & AndroidMediaShell are both for additional functionallity (BT remote via phone app, and Casting)

--- common/Android.mk
+++ common/Android.mk
@@ -204,6 +204,18 @@ LOCAL_PRIVILEGED_MODULE := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := SetupWraith
+LOCAL_MODULE_OWNER := gapps
+LOCAL_SRC_FILES := proprietary/priv-app/SetupWraith/SetupWraith.apk
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_DEX_PREOPT := false
+LOCAL_MODULE_SUFFIX := .apk
+LOCAL_PRIVILEGED_MODULE := true
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := TV
 LOCAL_MODULE_OWNER := gapps
 LOCAL_SRC_FILES := proprietary/priv-app/TV/TV.apk
